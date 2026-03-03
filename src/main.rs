94983b91afcc77c0dc724c34637961bbfe39c9c2
fabian-berencreutz <fabianberencreutz@gmail.com>
2026-03-03 11:07:51 +0100

    small fix

--- src/main.rs
+++ src/main.rs
@@ -53,118 +53,118 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> Result<()> 
         terminal
             .draw(|f| ui(f, app))
             .map_err(|e| IdeaError::Terminal(e.to_string()))?;
-        if event::poll(Duration::from_millis(100))? {
-            if let Event::Key(key) = event::read()? {
-                if app.mode == AppMode::ConfirmOpen {
-                    match key.code {
-                        KeyCode::Char('y') | KeyCode::Char('Y') | KeyCode::Enter => {
-                            app.execute_pending_open()?;
-                        }
-                        KeyCode::Char('n')
-                        | KeyCode::Char('N')
-                        | KeyCode::Esc
-                        | KeyCode::Backspace => {
-                            app.go_back();
-                        }
-                        _ => {}
+        if event::poll(Duration::from_millis(100))?
+            && let Event::Key(key) = event::read()?
+        {
+            if app.mode == AppMode::ConfirmOpen {
+                match key.code {
+                    KeyCode::Char('y') | KeyCode::Char('Y') | KeyCode::Enter => {
+                        app.execute_pending_open()?;
                     }
-                } else if app.mode == AppMode::Help {
-                    app.go_back();
-                } else if app.is_searching {
-                    match key.code {
-                        KeyCode::Enter => {
-                            app.is_searching = false;
-                        }
-                        KeyCode::Char(c) => {
-                            app.search_query.push(c);
-                            if let AppMode::CategorySelection | AppMode::CloneCategory = app.mode {
-                                app.category_state.select(Some(0));
-                            } else {
-                                app.project_state.select(Some(0));
-                            }
-                        }
-                        KeyCode::Backspace => {
-                            app.search_query.pop();
-                        }
-                        KeyCode::Esc => {
-                            app.is_searching = false;
-                            app.search_query.clear();
-                        }
-                        _ => {}
+                    KeyCode::Char('n')
+                    | KeyCode::Char('N')
+                    | KeyCode::Esc
+                    | KeyCode::Backspace => {
+                        app.go_back();
                     }
-                } else if app.mode == AppMode::InputUrl {
-                    match key.code {
-                        KeyCode::Enter => {
-                            app.on_enter()?;
+                    _ => {}
+                }
+            } else if app.mode == AppMode::Help {
+                app.go_back();
+            } else if app.is_searching {
+                match key.code {
+                    KeyCode::Enter => {
+                        app.is_searching = false;
+                    }
+                    KeyCode::Char(c) => {
+                        app.search_query.push(c);
+                        if let AppMode::CategorySelection | AppMode::CloneCategory = app.mode {
+                            app.category_state.select(Some(0));
+                        } else {
+                            app.project_state.select(Some(0));
                         }
-                        KeyCode::Char(c) => {
-                            app.input.push(c);
-                        }
-                        KeyCode::Backspace => {
-                            if app.input.is_empty() {
-                                app.mode = AppMode::MainMenu;
-                            } else {
-                                app.input.pop();
-                            }
-                        }
-                        KeyCode::Esc => {
+                    }
+                    KeyCode::Backspace => {
+                        app.search_query.pop();
+                    }
+                    KeyCode::Esc => {
+                        app.is_searching = false;
+                        app.search_query.clear();
+                    }
+                    _ => {}
+                }
+            } else if app.mode == AppMode::InputUrl {
+                match key.code {
+                    KeyCode::Enter => {
+                        app.on_enter()?;
+                    }
+                    KeyCode::Char(c) => {
+                        app.input.push(c);
+                    }
+                    KeyCode::Backspace => {
+                        if app.input.is_empty() {
                             app.mode = AppMode::MainMenu;
-                        }
-                        _ => {}
-                    }
-                } else if app.mode == AppMode::ChangeBaseDir {
-                    match key.code {
-                        KeyCode::Enter => {
-                            app.on_enter()?;
-                        }
-                        KeyCode::Char(c) => {
-                            app.input.push(c);
-                        }
-                        KeyCode::Backspace => {
+                        } else {
                             app.input.pop();
                         }
-                        KeyCode::Esc => {
-                            app.mode = AppMode::MainMenu;
-                        }
-                        _ => {}
                     }
-                } else {
-                    match key.code {
-                        KeyCode::Char('q') => return Ok(()),
-                        KeyCode::Char('f') => {
-                            app.toggle_favorite();
-                        }
-                        KeyCode::Char('t') => {
-                            app.open_terminal()?;
-                        }
-                        KeyCode::Char('r') => {
-                            app.refresh_current_view();
-                        }
-                        KeyCode::Char('/') => {
-                            if app.mode != AppMode::MainMenu && app.mode != AppMode::ThemeSelection
-                            {
-                                app.is_searching = true;
-                            }
-                        }
-                        KeyCode::Char('?') => {
-                            app.previous_mode = Some(app.mode.clone());
-                            app.mode = AppMode::Help;
-                        }
-                        KeyCode::Down | KeyCode::Char('j') => app.next(),
-                        KeyCode::Up | KeyCode::Char('k') => app.previous(),
-                        KeyCode::Enter | KeyCode::Right | KeyCode::Char('l') => {
-                            app.on_enter()?;
-                        }
-                        KeyCode::Left | KeyCode::Backspace | KeyCode::Char('h') => app.go_back(),
-                        KeyCode::Esc => {
-                            if !app.search_query.is_empty() {
-                                app.search_query.clear();
-                            } else {
-                                app.go_back();
-                            }
-                        }
-                        _ => {}
+                    KeyCode::Esc => {
+                        app.mode = AppMode::MainMenu;
                     }
+                    _ => {}
+                }
+            } else if app.mode == AppMode::ChangeBaseDir {
+                match key.code {
+                    KeyCode::Enter => {
+                        app.on_enter()?;
+                    }
+                    KeyCode::Char(c) => {
+                        app.input.push(c);
+                    }
+                    KeyCode::Backspace => {
+                        app.input.pop();
+                    }
+                    KeyCode::Esc => {
+                        app.mode = AppMode::MainMenu;
+                    }
+                    _ => {}
+                }
+            } else {
+                match key.code {
+                    KeyCode::Char('q') => return Ok(()),
+                    KeyCode::Char('f') => {
+                        app.toggle_favorite();
+                    }
+                    KeyCode::Char('t') => {
+                        app.open_terminal()?;
+                    }
+                    KeyCode::Char('r') => {
+                        app.refresh_current_view();
+                    }
+                    KeyCode::Char('/') => {
+                        if app.mode != AppMode::MainMenu && app.mode != AppMode::ThemeSelection
+                        {
+                            app.is_searching = true;
+                        }
+                    }
+                    KeyCode::Char('?') => {
+                        app.previous_mode = Some(app.mode.clone());
+                        app.mode = AppMode::Help;
+                    }
+                    KeyCode::Down | KeyCode::Char('j') => app.next(),
+                    KeyCode::Up | KeyCode::Char('k') => app.previous(),
+                    KeyCode::Enter | KeyCode::Right | KeyCode::Char('l') => {
+                        app.on_enter()?;
+                    }
+                    KeyCode::Left | KeyCode::Backspace | KeyCode::Char('h') => app.go_back(),
+                    KeyCode::Esc => {
+                        if !app.search_query.is_empty() {
+                            app.search_query.clear();
+                        } else {
+                            app.go_back();
+                        }
+                    }
+                    _ => {}
                 }
             }
         }
